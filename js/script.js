341a10ed162da39e5c1b82c0426acabe714b0955
Kasim Siddiqui <kasimsiddiqui@gmail.com>
2015-07-21 10:31:43 -0700

    moved ready

--- js/script.js
+++ js/script.js
@@ -1,81 +1,80 @@
-var Photo = function(url){
-  this.url = url;
-}
+$(document).ready(function(){
 
-var Tracker = function(){
-  this.photos = [];
-}
+  var Photo = function(url){
+    this.url = url;
+  }
 
-Tracker.prototype.generateRandom = function(numPhotos) {
-  return Math.floor(Math.random() * numPhotos);
-};
+  var Tracker = function(){
+    this.photos = [];
+  }
 
-Tracker.prototype.pickRandomPhoto = function(){
-  return this.generateRandom(this.photos.length);
-};
+  Tracker.prototype.generateRandom = function(numPhotos) {
+    return Math.floor(Math.random() * numPhotos);
+  };
 
-Tracker.prototype.renderRandomPhoto = function() {
-  var random = this.pickRandomPhoto();
-  var photo = '<img src="' + this.photos[random][0].url + '" />' + this.photos[random][1];
-  return photo;
-};
+  Tracker.prototype.pickRandomPhoto = function(){
+    return this.generateRandom(this.photos.length);
+  };
 
-Tracker.prototype.addPhoto = function(photo) {
-  var photoArray = [photo,0];
-  this.photos.push(photoArray);
-};
+  Tracker.prototype.renderRandomPhoto = function() {
+    var random = this.pickRandomPhoto();
+    var photo = '<img src="' + this.photos[random][0].url + '" />' + this.photos[random][1];
+    return photo;
+  };
 
-Tracker.prototype.renderComparison = function(){
-  var photo1 = this.renderRandomPhoto();
-  var photo2 = this.renderRandomPhoto();
-  while(photo1 == photo2){
-    console.log("the photos were the same!");
+  Tracker.prototype.addPhoto = function(photo) {
+    var photoArray = [photo,0];
+    this.photos.push(photoArray);
+  };
+
+  Tracker.prototype.renderComparison = function(){
     var photo1 = this.renderRandomPhoto();
     var photo2 = this.renderRandomPhoto();
-  }
-  $('#photo1').html(photo1);
-  $('#photo2').html(photo2);
-  highlight();
-};
-
-Tracker.prototype.findPhotoInArray = function(url) {
-  console.log(url);
-  for(var i = 0; i < this.photos.length; i++){
-    if(this.photos[i][0].url == url){
-      return i;
+    while(photo1 == photo2){
+      console.log("the photos were the same!");
+      var photo1 = this.renderRandomPhoto();
+      var photo2 = this.renderRandomPhoto();
     }
-  }
-  return false;
-};
+    $('#photo1').html(photo1);
+    $('#photo2').html(photo2);
+    highlight();
+  };
 
-Tracker.prototype.incrementVote = function(photo){
-  var index = this.findPhotoInArray(photo);
-  this.photos[index][1]++;
-};
+  Tracker.prototype.findPhotoInArray = function(url) {
+    console.log(url);
+    for(var i = 0; i < this.photos.length; i++){
+      if(this.photos[i][0].url == url){
+        return i;
+      }
+    }
+    return false;
+  };
 
-$(document).ready(function(){
+  Tracker.prototype.incrementVote = function(photo){
+    var index = this.findPhotoInArray(photo);
+    this.photos[index][1]++;
+  };
+
   $('button').click(function(){
     testTracker.incrementVote($('.highlight').attr('src'));
     $('button').css({'visibility':'hidden'});
     testTracker.renderComparison();
   });
-});
 
-var highlight = function(){
-  $(document).ready(function(){
+  var highlight = function(){
     $('img').click(function(){
-    $('button').css({'visibility':'visible'});
-    $('.highlight').removeClass('highlight');
-    $(this).toggleClass('highlight');
+      $('button').css({'visibility':'visible'});
+      $('.highlight').removeClass('highlight');
+      $(this).toggleClass('highlight');
     });
-  });
-}
+  }
 
-var testTracker = new Tracker();
-testTracker.generateRandom(14);
-for(var i = 0; i <= 13; i++) {
-  var photoURL = "img/" + i + ".jpg";
-  var photo = new Photo(photoURL);
-  testTracker.addPhoto(photo);
-};
-testTracker.renderComparison();
+  var testTracker = new Tracker();
+  testTracker.generateRandom(14);
+  for(var i = 0; i <= 13; i++) {
+    var photoURL = "img/" + i + ".jpg";
+    var photo = new Photo(photoURL);
+    testTracker.addPhoto(photo);
+  };
+  testTracker.renderComparison();
+});
